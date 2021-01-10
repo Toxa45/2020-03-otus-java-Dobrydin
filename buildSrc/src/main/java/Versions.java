bd805419980de21035e33f2678ab8269cca16823
Dobrydin Anton <A.Dobrydin@hostco.ru>
2021-01-10 14:47:31 +0500

    add dependency

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,4 +1,8 @@
 public interface Versions {
     String guava = "28.2-jre";
     String glassfishJson = "1.1.4";
+    String flyway = "6.4.4";
+    String testcontainers = "1.14.3";
+    String postgresql = "42.2.5";
+    String lombok = "1.18.16";
 }
